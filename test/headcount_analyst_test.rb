6223c697d052b7f2452a53ecd4d642febb4a7098
Tyler Komoroske <tjkomor@clemson.edu>
2015-09-23 14:06:00 -0600

    small updates to analyst class

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -5,8 +5,8 @@ require './lib/headcount_analyst'
 class HeadcountAnalystTest < Minitest::Test
 
   def test_it_can_find_a_single_subject_leader
-    ha = HeadcountAnalyst.new
-    assert_equal '', ha.top_statewide_testing_year_over_year_growth(:subject => :math)
+    ha = HeadcountAnalyst.get_data
+    assert_equal '', ha.data
   end
 
 end
