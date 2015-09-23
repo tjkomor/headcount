ff4248a2e4775cee3f0eaf4fa2d96165f56f4fda
cminnerath <cminnerath@gmail.com>
2015-09-22 19:16:31 -0600

    two of three initial tests passing

--- lib/enrollment.rb
+++ lib/enrollment.rb
@@ -3,4 +3,12 @@ class Enrollment
   def initialize(data)
     @data = data
   end
+
+  def in_year(year)
+    @data[:enrollment][:participation_by_year][year.to_s.to_sym]
+  end
+
+  def graduation_in_year(year)
+    @data[:enrollment][:graduation_rate_by_year][year.to_s.to_sym]
+  end
 end
--- test/district_repository_test.rb
+++ test/district_repository_test.rb
@@ -10,7 +10,7 @@ class DistrictRepositoryTest < Minitest::Test
    district = dr.find_by_name("ACADEMY 20")
 
    assert_equal 22620, district.enrollment.in_year(2009)
-   assert_equal 0.895, district.enrollment.graduation_rate.for_high_school_in_year(2010)
+   assert_equal 0.895, district.enrollment.graduation_in_year(2010)
    assert_equal 0.857, district.statewide_testing.proficient_for_subject_by_grade_in_year(:math, 3, 2008)
  end
 end
